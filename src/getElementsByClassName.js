5272443dbe4671943c7b59666e72a348100c1b2c
Austin <ariggs7@gmail.com>
2021-09-20 21:27:52 -0500

    Resursion Review Minimum Complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, element) {
+  // Input - className "string"
+  // Output - array like structure containing all nodes with corresponding classname.
+
+  // return result array thing
+  var nodeSearch = element || document.body;
+  var result = [];
+
+  // if the node / name exists and name cotains classname
+  // push node into results
+
+  // check if node has childern
+  // if it does, call getElementByClassName on them. & concat
+
+  if (nodeSearch.classList && nodeSearch.classList.contains(className)) {
+    result.push(nodeSearch);
+  }
+
+  if (nodeSearch.childNodes) {
+    for (var i = 0; i < nodeSearch.childNodes.length; i++) {
+      result = result.concat(getElementsByClassName(className, nodeSearch.childNodes[i]));
+    }
+  }
+
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // input - any value
+  // JSON.stringify output of value
+
+  var result = [];
+
+  if (obj === null) {
+    return '' + obj + '';
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (typeof obj === 'number') {
+    return '' + obj + '';
+  }
+
+  if (typeof obj === 'boolean') {
+    return '' + obj + '';
+  }
+
+  // If obj is array, then
+  if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result + ']';
+  }
+
+  if (!Array.isArray(obj) && typeof obj === 'object') {
+    // loop through each key value pair and stringify all keys
+    var stringedObj = '';
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        stringedObj += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    // recurse for each value of key
+    stringedObj = stringedObj.substring(0, stringedObj.length - 1);
+    return '{' + stringedObj + '}';
+  }
+  // iterate through each value and push into result
+  // then we recurseif !Array.isArray && typeof === object
 };
